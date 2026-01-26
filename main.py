3a932505d689eef777614e8d84c80aebfbca6cf3
mel chibnik <mel.chibnik@gmail.com>
2026-01-25 21:03:12 -0500

    changing render URL

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://melchib.github.io"],
+    allow_origins=[
+        "https://melchib.github.io",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 @app.post("/upload")
